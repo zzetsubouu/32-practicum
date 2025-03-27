39ca237ae1251e8fee69ee71153fb8c70b622b8a
Kab-35-16 <Kab-35-16@KAB-31-8->
2025-03-27 11:22:49 +0200

    .

--- 32-practicum/Person.cs
+++ 32-practicum/Person.cs
@@ -8,6 +8,26 @@ namespace _32_practicum
 {
     abstract class Person
     {
+        private string firstName;
+        private string lastName;
+        private DateTime dateOfBirth;
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public DateTime DateOfBirth { get => dateOfBirth; set => dateOfBirth = value; }
+        public Person(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+        }
+        public abstract void PrintInfo();
+        //переделать метод 
+        //public int GetAge()
+        //{
+        //    DateTime now = DateTime.Now;
+        //    DateTime age = now.Year - DateOfBirth.Year;
+        //    return age;
+        //}
 
     }
 }
